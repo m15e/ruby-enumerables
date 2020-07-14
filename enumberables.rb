1dc0aa7d44b2d5e2c164974b176318efaf043fc4
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-07-14 22:50:23 +0800

    ADD my_each_with_index method

--- enumberables.rb
+++ enumberables.rb
@@ -8,5 +8,16 @@ module Enumerable
       puts 'This method needs to be called on arrays only'
     end
   end
+
+  def my_each_with_index
+    if self.kind_of?(Array)
+      self.length.times do |i|
+        yield(self[i], i)
+      end
+    else
+      puts 'This method needs to be called on arrays only'
+    end
+  end
   
+  [4,2313,2,2,'Blip'].my_each_with_index {|v, i| puts "val: #{v}, idx: #{i}"}
 end
