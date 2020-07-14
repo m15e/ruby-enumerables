acd9a064d65678860cd5ba72777bb38d4adb7d1a
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-07-14 23:40:01 +0800

    ADD partial my_none solution)

--- enumberables.rb
+++ enumberables.rb
@@ -60,13 +60,40 @@ module Enumerable
       puts 'This method needs to be called on arrays only'
     end
   end
+
+  def my_none
+    if self.kind_of?(Array)
+      all_true = true
+      self.my_each do |condition|
+        if yield(condition) == true
+          all_true = false
+        end
+      end
+      return all_true
+    else
+      puts 'This method needs to be called on arrays only'
+    end
+  end
   
   
 end
 
-p [10,19,5].my_any { |num| num > 13 }
+# %w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+# %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
+# %w{ant bear cat}.none?(/d/)                        #=> true
+# [1, 3.14, 42].none?(Float)                         #=> false
+# [].none?                                           #=> true
+# [nil].none?                                        #=> true
+# [nil, false].none?                                 #=> true
+# [nil, false, true].none?                           #=> false
 
-p [10,19, 5].any? { |num| num > 13 }
+# p [1, 3.14, 42].none?(Float) # => returns true
+
+p [nil, false, true].my_none {}
+
+#p [].my_none { |num| num.kind_of?(Float) } # => false
+
+#p ['ant', 'bear', 'cat'].none? { |word| word.length >= 4 }
 
 # [1,2,3,20,30,40] array.select { |num| num > 10 }
 
