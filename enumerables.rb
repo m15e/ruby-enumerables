f578cc590d4c16dbc1234789afa0940edd4ea90b
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 02:34:18 +0200

    implement requested feedback

--- enumerables.rb
+++ enumerables.rb
@@ -5,21 +5,25 @@
 # rubocop:disable Metrics/PerceivedComplexity
 module Enumerable
   def my_each
-    arr = self
-    return arr.to_enum unless block_given?
+    arr_default = self
+    return arr_default.to_enum unless block_given?
 
-    arr = arr.is_a?(Array) ? arr : arr.to_a
-    arr.length.times { |i| yield(arr[i]) }
-    arr
+    arr_converted = arr_default.is_a?(Array) ? arr_default : arr_default.to_a
+    arr_converted.length.times { |i| yield(arr_converted[i]) }
+    return arr_default unless arr_default.is_a?(Array)
+
+    arr_converted
   end
 
   def my_each_with_index
-    arr = self
-    return arr.to_enum unless block_given?
+    arr_default = self
+    return arr_default.to_enum unless block_given?
 
-    arr = arr.is_a?(Array) ? arr : arr.to_a
-    arr.length.times { |i| yield(arr[i], i) }
-    arr
+    arr_converted = arr_default.is_a?(Array) ? arr_default : arr_default.to_a
+    arr_converted.length.times { |i| yield(arr_converted[i], i) }
+    return arr_default unless arr_default.is_a?(Array)
+
+    arr_converted
   end
 
   def my_select
@@ -111,6 +115,8 @@ module Enumerable
   def my_inject(num = nil, symb = nil)
     arr = self
     arr = arr.is_a?(Array) ? arr : arr.to_a
+    return raise LocalJumpError, 'no block given' if !block_given? and num.nil? and symb.nil?
+
     if num.nil? and symb.nil?
       accumulator = arr[0]
       if block_given?
@@ -175,3 +181,47 @@ end
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/MethodLength
 # rubocop:enable Metrics/PerceivedComplexity
+
+##################################
+# ARRAY_SIZE = 100
+# LOWEST_VALUE = 0
+# HIGHEST_VALUE = 9
+
+# describe 'enumerables' do
+#   let(:array) { Array.new(ARRAY_SIZE) { rand(LOWEST_VALUE...HIGHEST_VALUE) } }
+#   let(:block) { proc { |num| num < (LOWEST_VALUE + HIGHEST_VALUE) / 2 } }
+#   let(:words) { %w[dog door rod blade] }
+#   let(:range) { Range.new(5, 50) }
+#   let(:hash) { { a: 1, b: 2, c: 3, d: 4, e: 5 } }
+#   let(:numbers) { [1, 2i, 3.14] }
+#   let!(:array_clone) { array.clone }
+
+# Sum some numbers
+# p (5..10).inject                         #=> 45
+# Same using a block and inject
+# p (5..10).inject { |sum, n| sum + n }            #=> 45
+# Multiply some numbers
+# p (5..10).inject                          #=> 151200
+# # Same using a block
+# p (5..10).inject(1) { |product, n| product * n } #=> 151200
+# # find the longest word
+# longest = %w{ cat sheep bear }.inject do |memo, word|
+#    memo.length > word.length ? memo : word
+# end
+# p longest                                        #=> "sheep"
+
+puts "============================================"
+
+# Sum some numbers
+p (5..10).my_inject                            #=> 45
+# Same using a block and inject
+# p (5..10).my_inject { |sum, n| sum + n }            #=> 45
+# # Multiply some numbers
+# p (5..10).my_inject(1, :*)                          #=> 151200
+# # Same using a block
+# p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
+# # find the longest word
+# longest = %w{ cat sheep bear }.my_inject do |memo, word|
+#    memo.length > word.length ? memo : word
+# end
+# p longest                                        #=> "sheep"
