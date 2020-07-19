3d7ba74b51f03479be1a9e12180178f3841d86a6
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 02:35:42 +0200

    remove commented codes

--- enumerables.rb
+++ enumerables.rb
@@ -181,17 +181,3 @@ end
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/MethodLength
 # rubocop:enable Metrics/PerceivedComplexity
-
-##################################
-# ARRAY_SIZE = 100
-# LOWEST_VALUE = 0
-# HIGHEST_VALUE = 9
-
-# describe 'enumerables' do
-#   let(:array) { Array.new(ARRAY_SIZE) { rand(LOWEST_VALUE...HIGHEST_VALUE) } }
-#   let(:block) { proc { |num| num < (LOWEST_VALUE + HIGHEST_VALUE) / 2 } }
-#   let(:words) { %w[dog door rod blade] }
-#   let(:range) { Range.new(5, 50) }
-#   let(:hash) { { a: 1, b: 2, c: 3, d: 4, e: 5 } }
-#   let(:numbers) { [1, 2i, 3.14] }
-#   let!(:array_clone) { array.clone }
